f65b997e4daff2b8ef44187ad384a63554bf08df
Stanislav Salnikov <delfi89@gmail.com>
2022-11-18 18:19:20 +0100

    chore: add postcss plugins
    add postcss autoprefixer plugin
    add postcss-nested plugin
    add postcss-simple-vars plugin
    add postcss-custom-properties plugin

--- packages/client/vite.config.ts
+++ packages/client/vite.config.ts
@@ -1,6 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import dotenv from 'dotenv'
+import autoprefixer from 'autoprefixer'
+
 dotenv.config()
 
 // https://vitejs.dev/config/
