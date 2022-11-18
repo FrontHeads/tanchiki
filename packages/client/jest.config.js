376399bb5f09d650f9bcde5288cef47de093216c
Stanislav Salnikov <delfi89@gmail.com>
2022-11-18 19:19:21 +0100

    fix: fix the bug with the tests
    add rule to ignore style files during tests

--- packages/client/jest.config.js
+++ packages/client/jest.config.js
@@ -5,6 +5,9 @@ export default {
   preset: 'ts-jest',
   testEnvironment: 'jsdom',
   testMatch: ['<rootDir>/src/**/*.test.{ts,tsx}'],
+  moduleNameMapper: {
+    '\\.(css|less|scss|sass)$': 'identity-obj-proxy',
+  },
   globals: {
     __SERVER_PORT__: process.env.SERVER_PORT,
   },
