c3f9b275fb0f53b6488140cfe6a2d5cad70bedbc
Dmitry Gramoteev <gramoteev@gmail.com>
2022-11-25 12:46:14 +0500

    refactor(Root): clean styles

--- packages/client/src/layouts/Root/Root.tsx
+++ packages/client/src/layouts/Root/Root.tsx
@@ -7,20 +7,6 @@ export const Root: React.FC = () => {
 
   return (
     <div className={['layout', `layout_state_${navigation.state}`].join(' ')}>
-      <nav className="layout__nav">
-        <ul className="layout__nav-list">
-          <li className="layout__nav-item">
-            <NavLink className="layout__nav-link" to={`/`}>
-              App
-            </NavLink>
-          </li>
-          <li className="layout__nav-item">
-            <NavLink className="layout__nav-link" to={`/hello/FrontHeads`}>
-              Hello
-            </NavLink>
-          </li>
-        </ul>
-      </nav>
       <main className="layout__main">
         <Outlet />
       </main>
