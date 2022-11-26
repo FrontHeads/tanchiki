c257f96182a752c6aaf6ae4dbad04809a9a64e10
Optimizitor <sapomaro@gmail.com>
2022-11-26 22:50:08 +0300

    feat: add game typings

---
+++ packages/client/src/game/typings/index.ts
@@ -0,0 +1,8 @@
+export type DirectionT = 'UP' | 'DOWN' | 'LEFT' | 'RIGHT';
+export type RectT = {
+  posX: number;
+  posY: number;
+  width: number;
+  height: number;
+};
+export type PosStateT = { hasCollision: boolean };
