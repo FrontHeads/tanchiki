d1c84c0781fa5a0bd186169e7cae628ba4a18624
Optimizitor <sapomaro@gmail.com>
2022-11-26 22:52:29 +0300

    feat: add game to home page

--- packages/client/src/pages/Home/Home.tsx
+++ packages/client/src/pages/Home/Home.tsx
@@ -2,6 +2,8 @@ import './Home.css';
 
 import { useEffect } from 'react';
 
+import { Tanchiki } from '../../game';
+
 export const Home = () => {
   useEffect(() => {
     const fetchServerData = async () => {
@@ -12,10 +14,15 @@ export const Home = () => {
     };
 
     fetchServerData();
+
+    const game = new Tanchiki(document.querySelector('#tanchiki-game'));
+    game.init();
   }, []);
+
   return (
     <div className="app">
       <div className="text-center">Вот тут будет жить ваше приложение :)</div>
+      <div id="tanchiki-game" style={{ margin: '10px 0', border: '10px solid grey' }}></div>
     </div>
   );
 };
