db194c36c0664e6d6c1e7e32fc46af6401747217
Optimizitor <sapomaro@gmail.com>
2022-11-28 16:35:52 +0300

    test: add tests for EventBus, Controller & Zone

---
+++ packages/client/src/game/utils/EventBus.test.ts
@@ -0,0 +1,25 @@
+import {EventBus} from './';
+
+describe('utils/EventBus', () => {
+  it('should subscribe to and publish custom events', () => {
+    const eventBus = new EventBus();
+    const mockFn = jest.fn();
+
+    eventBus.on('mockEvent', mockFn);
+    eventBus.emit('mockEvent', 'mockParam');
+
+    expect(mockFn).toBeCalledTimes(1);
+    expect(mockFn).toHaveBeenCalledWith('mockParam');
+  });
+
+  it('should unsubscribe from custom events', () => {
+    const eventBus = new EventBus();
+    const mockFn = jest.fn();
+
+    eventBus.on('mockEvent', mockFn);
+    eventBus.off('mockEvent', mockFn);
+    eventBus.emit('mockEvent', 'mockParam');
+
+    expect(mockFn).not.toHaveBeenCalled();
+  });
+});
