be12e2038902cd8b205d4bd123e0cc630f4103a4
andreyatx <mitrofanobandrei@gmail.com>
2022-11-28 20:32:10 +0300

    feat(pages): add sign up page (no logic)

--- packages/client/src/config/router.tsx
+++ packages/client/src/config/router.tsx
@@ -6,6 +6,7 @@ import { Hello } from '../pages/Hello';
 import { Home } from '../pages/Home';
 import { Leaderboard } from '../pages/Leaderboard';
 import { SignIn } from '../pages/SignIn';
+import { SignUp } from '../pages/SignUp';
 import { Paths } from './constants';
 
 export const router = createBrowserRouter([
@@ -20,6 +21,10 @@ export const router = createBrowserRouter([
       {
         path: Paths.SignIn,
         element: <SignIn />,
+      },
+      {
+        path: Paths.SignUp,
+        element: <SignUp />,
       },
       {
         path: 'hello/:topic',
---
+++ packages/client/src/pages/SignUp/SignUp.tsx
@@ -0,0 +1,97 @@
+import { FC, useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+
+import { Button } from '../../components/Button';
+import { Form } from '../../components/Form';
+import { FormField } from '../../components/FormField';
+import { Paths } from '../../config/constants';
+import { SignUpForm } from './typings';
+
+export const SignUp: FC = () => {
+  const navigate = useNavigate();
+  const formData: SignUpForm = {
+    email: '',
+    login: '',
+    first_name: '',
+    last_name: '',
+    phone: '',
+    password: '',
+    password_check: '',
+  };
+  const [responseBody, setResponseBody] = useState<SignUpForm>(formData);
+
+  const inputChangeHandler = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const { name, value } = event.target;
+    setResponseBody({ ...responseBody, [name]: value });
+  };
+
+  const submitHandler = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    console.log(responseBody);
+  };
+
+  return (
+    <Form handlerSubmit={submitHandler} header="Вход">
+      <FormField
+        title="Email"
+        type="email"
+        id="email"
+        onChange={inputChangeHandler}
+        placeholder="ivanIvanov@yandex.ru"
+        required={true}
+      />
+      <FormField
+        title="Логин"
+        type="login"
+        id="login"
+        onChange={inputChangeHandler}
+        placeholder="ivanIvanov"
+        required={true}
+      />
+      <FormField
+        title="Имя"
+        type="first_name"
+        id="first_name"
+        onChange={inputChangeHandler}
+        placeholder="Иван"
+        required={false}
+      />
+      <FormField
+        title="Фамилия"
+        type="last_name"
+        id="last_name"
+        onChange={inputChangeHandler}
+        placeholder="Иванов"
+        required={false}
+      />
+      <FormField
+        title="Телефон"
+        type="phone"
+        id="phone"
+        onChange={inputChangeHandler}
+        placeholder="+7 800 555 35 35"
+        required={false}
+      />
+      <FormField
+        title="Пароль"
+        type="password"
+        id="password"
+        onChange={inputChangeHandler}
+        placeholder="Латинские буквы и цифры"
+        required={true}
+      />
+      <FormField
+        title="Повторите пароль"
+        type="password_check"
+        id="password_check"
+        onChange={inputChangeHandler}
+        placeholder="Латинские буквы и цифры"
+        required={true}
+      />
+      <div className="form__buttons-wrapper">
+        <Button text="Зарегистрироваться" type="submit" selector="button_primary" />
+        <Button text="Вход" onClick={() => navigate(Paths.SignIn)} selector="button_secondary" />
+      </div>
+    </Form>
+  );
+};
