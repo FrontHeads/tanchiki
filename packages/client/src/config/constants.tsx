f68405cb1efe819d4ee59e421583858dd2843408
Dmitry Gramoteev <gramoteev@gmail.com>
2022-11-28 23:04:08 +0500

    feat(Footer): add footer component

--- packages/client/src/config/constants.tsx
+++ packages/client/src/config/constants.tsx
@@ -1,6 +1,7 @@
 export enum Paths {
   SignIn = '/sign-in',
   SignUp = '/sign-up',
+  Game = '/game',
 }
 
 export const API_HOST = 'https://ya-praktikum.tech/api/v2';
--- packages/client/src/layouts/Root/Root.tsx
+++ packages/client/src/layouts/Root/Root.tsx
@@ -2,12 +2,17 @@ import './Root.css';
 
 import { Outlet, useNavigation } from 'react-router-dom';
 
+import { Footer } from '../../components/Footer';
+import { Paths } from '../../config/constants';
+
 export const Root: React.FC = () => {
   const navigation = useNavigation();
 
   return (
     <main className={['layout', `layout_state_${navigation.state}`].join(' ')}>
+      <header>Hello</header>
       <Outlet />
+      {navigation.location?.pathname !== Paths.Game && <Footer />}
     </main>
   );
 };
