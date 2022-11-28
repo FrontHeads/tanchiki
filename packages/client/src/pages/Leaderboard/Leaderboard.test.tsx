ee6ae71f83e6a2906f30af8f2ec36994e147285e
andreyatx <mitrofanobandrei@gmail.com>
2022-11-28 12:02:56 +0300

    refactor(test): commit for test update on github

--- packages/client/src/pages/Leaderboard/Leaderboard.test.tsx
+++ packages/client/src/pages/Leaderboard/Leaderboard.test.tsx
@@ -3,7 +3,7 @@ import '@testing-library/jest-dom';
 import { render, screen } from '@testing-library/react';
 
 import { headerText, Leaderboard } from './Leaderboard';
-
+//for test update
 describe('Leaderboard', () => {
   test('it renders', () => {
     render(<Leaderboard header={headerText} />);
