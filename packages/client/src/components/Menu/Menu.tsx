cf291c3a28c9e42c5731031fa65e47802a8cca87
andreyatx <mitrofanobandrei@gmail.com>
2022-11-29 20:08:26 +0300

    feat(client): add classnames package
    change some classnames

--- packages/client/src/components/Menu/Menu.tsx
+++ packages/client/src/components/Menu/Menu.tsx
@@ -1,5 +1,6 @@
 import './Menu.css';
 
+import cn from 'classnames';
 import { FC, useEffect, useRef, useState } from 'react';
 
 import { MenuLink } from '../MenuLink';
@@ -8,6 +9,14 @@ import { navigationList } from './MenuData';
 export const Menu: FC = () => {
   const [isOpen, setIsOpen] = useState(false);
   const menuRef = useRef<HTMLDivElement>(null);
+
+  const iconLineClassName = cn('menu__icon-line ', {
+    'menu__icon-line_open': isOpen,
+  });
+
+  const menuListClassName = cn('menu__list ', {
+    menu__list_hide: !isOpen,
+  });
 
   const toggleMenu = () => {
     setIsOpen(prevState => {
@@ -23,23 +32,20 @@ export const Menu: FC = () => {
     const handler = (e: MouseEvent) => {
       if (!menuRef.current?.contains(e.target as HTMLElement)) {
         setIsOpen(false);
-        console.log(menuRef.current);
       }
     };
     document.addEventListener('mousedown', handler);
+    return () => document.removeEventListener('mousedown', handler);
   });
 
   return (
     <div data-testid="menu" className="menu" ref={menuRef}>
       <div data-testid="menu__icon" className="menu__icon" onClick={toggleMenu}>
-        <div className={'menu__icon-line ' + (isOpen ? 'menu__icon-line_open' : '')}></div>
-        <div className={'menu__icon-line ' + (isOpen ? 'menu__icon-line_open' : '')}></div>
-        <div className={'menu__icon-line ' + (isOpen ? 'menu__icon-line_open' : '')}></div>
+        <div className={iconLineClassName}></div>
+        <div className={iconLineClassName}></div>
+        <div className={iconLineClassName}></div>
       </div>
-      <div
-        data-testid="menu__list"
-        data-test={isOpen ? 'menu-list-on' : 'menu-list-off'}
-        className={'menu__list ' + (isOpen ? '' : 'menu__list_hide')}>
+      <div data-testid="menu__list" data-test={isOpen ? 'menu-list-on' : 'menu-list-off'} className={menuListClassName}>
         <nav className="menu-nav">
           <ul className="navigation-list">
             {navigationList.map(({ id, title, to }) => (
