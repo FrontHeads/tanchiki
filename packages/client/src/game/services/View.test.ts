c4783474d9ac3875bc59d683dab2de9180f45fb1
Optimizitor <sapomaro@gmail.com>
2022-11-29 10:37:43 +0300

    test: update test for View class

--- packages/client/src/game/services/View.test.ts
+++ packages/client/src/game/services/View.test.ts
@@ -6,10 +6,6 @@ import { View } from './';
 function mockEntity(rect: RectT) {
   const entity = new EventBus() as Entity;
   Object.assign(entity, rect);
-  entity.alignedToGrid = true;
-  entity.lastRect = rect;
-  entity.getRect = () => rect;
-  entity.nextRect = rect;
   return entity;
 }
 
@@ -20,23 +16,31 @@ describe('game/services/View', () => {
     expect(document.querySelector('canvas')).not.toBe(null);
   });
 
-  it('should bind entities to layers', () => {
+  it('should bind entities to layers and subscribe to the updates', () => {
     const view = new View({ width: 10, height: 10 }, null);
-    const entity = { on: () => null } as unknown as Entity;
-
-    view.bindEntityToLayer(entity, 'tanks');
-
-    expect(view.layers['tanks'].entities.has(entity)).toBe(true);
-  });
-
-  it('should remove entities from layers', () => {
-    const view = new View({ width: 10, height: 10 }, null);
+    view.drawEntityOnLayer = jest.fn();
+    view.eraseEntityFromLayer = jest.fn();
     const entity = mockEntity({ posX: 2, posY: 2, width: 2, height: 2 });
 
     view.bindEntityToLayer(entity, 'tanks');
-    view.removeEntityFromLayer(entity, 'tanks');
+    entity.emit('entityShouldUpdate');
+    entity.emit('entityDidUpdate');
+
+    expect(view.layers['tanks'].entities.has(entity)).toBe(true);
+    expect(view.eraseEntityFromLayer).toHaveBeenCalledTimes(1);
+    expect(view.drawEntityOnLayer).toHaveBeenCalledTimes(1);
+  });
+
+  it('should subscribe to entity destruction', () => {
+    const view = new View({ width: 10, height: 10 }, null);
+    view.eraseEntityFromLayer = jest.fn();
+    const entity = mockEntity({ posX: 2, posY: 2, width: 2, height: 2 });
+
+    view.bindEntityToLayer(entity, 'tanks');
+    entity.emit('entityShouldBeDestroyed');
 
     expect(view.layers['tanks'].entities.has(entity)).toBe(false);
+    expect(view.eraseEntityFromLayer).toHaveBeenCalled();
   });
 
   it('should calculate entity rect in pixels', () => {
