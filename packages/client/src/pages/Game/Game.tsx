3068042a45d2bf376171104cad76192e4c08b404
Optimizitor <sapomaro@gmail.com>
2022-11-30 12:48:55 +0300

    refactor: use ref to set game root element

--- packages/client/src/pages/Game/Game.tsx
+++ packages/client/src/pages/Game/Game.tsx
@@ -1,17 +1,19 @@
-import { useEffect } from 'react';
+import { useEffect, useRef } from 'react';
 
 import { Tanchiki } from '../../game';
 
 export const Game = () => {
+  const gameRoot = useRef(null);
+
   useEffect(() => {
-    const game = new Tanchiki(document.querySelector('#tanchiki-game'));
+    const game = new Tanchiki(gameRoot.current);
     game.init();
   }, []);
 
   return (
     <section style={{ margin: '0 auto' }}>
       <h2>Заглушка для страницы с игрой</h2>
-      <div id="tanchiki-game" style={{ border: '10px solid grey' }}></div>
+      <div ref={gameRoot} style={{ border: '10px solid grey' }}></div>
     </section>
   );
 };
