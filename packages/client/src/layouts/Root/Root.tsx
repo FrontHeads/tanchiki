167d6860a46039840c2bdae21843d7fffaa7c825
DmitriiNazimov <kurs-ssu@mail.ru>
2022-11-30 19:28:06 +0300

    refactor(ErrorPage): add layout and implement design

--- packages/client/src/layouts/Root/Root.tsx
+++ packages/client/src/layouts/Root/Root.tsx
@@ -1,16 +1,19 @@
 import './Root.css';
 
+import { FC } from 'react';
 import { Outlet, useNavigation } from 'react-router-dom';
 
 import { Menu } from '../../components/Menu';
+import { RootProps } from './typings';
 
-export const Root: React.FC = () => {
+export const Root: FC<RootProps> = ({ children }) => {
   const navigation = useNavigation();
 
   return (
     <main className={['layout', `layout_state_${navigation.state}`].join(' ')}>
       <Menu />
       <Outlet />
+      {children}
     </main>
   );
 };
--- packages/client/src/pages/ErrorPage/ErrorPage.tsx
+++ packages/client/src/pages/ErrorPage/ErrorPage.tsx
@@ -1,17 +1,32 @@
 import './ErrorPage.css';
 
-import { Link, useRouteError } from 'react-router-dom';
+import { FC } from 'react';
+import { Link, useNavigate, useRouteError } from 'react-router-dom';
 
-type ErrorType = { status?: number; statusText?: string; message?: string };
+import { Button } from '../../components/Button';
+import { Paths } from '../../config/constants';
+import { Root } from '../../layouts/Root';
+import { ErrorType } from './typings';
 
-export const ErrorPage: React.FC = () => {
+export const ErrorPage: FC = () => {
   const error = useRouteError() as ErrorType;
+  const navigate = useNavigate();
+
+  let messageText = error.statusText || error.message || 'Возникла ошибка';
+
+  if (error.statusText === 'Not Found') {
+    messageText = 'Не туда попали';
+  }
 
   return (
-    <div className="error-page">
-      <h1>Ошибка {error.status}!</h1>
-      <p>{error.statusText || error.message}</p>
-      <Link to={'/'}>На главную</Link>
-    </div>
+    <Root>
+      <div className="web-error">
+        <h1 className="web-error__paragraph web-error__header">{error.status}</h1>
+        <p className="web-error__paragraph web-error__msg">{messageText}</p>
+        <p className="web-error__paragraph web-error__link">
+          <Button text="На главную" onClick={() => navigate(Paths.Home)} selector="button_secondary" />
+        </p>
+      </div>
+    </Root>
   );
 };
