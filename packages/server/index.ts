a40cbacd7d8cfd3f0fce2d26bc0996bbf5e2f1b6
Stanislav <delfi89@gmail.com>
2022-12-01 09:24:33 +0100

    fix(cors): change server CORS settings (#18)

--- packages/server/index.ts
+++ packages/server/index.ts
@@ -7,8 +7,16 @@ import { createClientAndConnect } from './db';
 dotenv.config();
 
 const app = express();
-app.use(cors());
+
+const clientPort = Number(process.env.CLIENT_PORT) || 3000;
 const port = Number(process.env.SERVER_PORT) || 3001;
+const corsOptions = {
+  credentials: true,
+  origin: `http://127.0.0.1:${clientPort}`,
+  optionsSuccessStatus: 200, // some legacy browsers (IE11, various SmartTVs) choke on 204
+};
+
+app.use(cors(corsOptions));
 
 createClientAndConnect();
 
