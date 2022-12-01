376bc80bb4ddcf2767c7537a238dc0b04b4a905c
DmitriiNazimov <kurs-ssu@mail.ru>
2022-12-01 10:42:51 +0300

    refactor(Button): update button classnames constructor

--- packages/client/src/components/Button/Button.tsx
+++ packages/client/src/components/Button/Button.tsx
@@ -3,15 +3,17 @@ import './Button.css';
 import cn from 'classnames';
 import { FC } from 'react';
 
-import { ButtonProps } from './typings';
+import { ButtonProps, ButtonVariant } from './typings';
 
-export const Button: FC<ButtonProps> = ({ type = 'button', text, className, variant, testId, ...rest }) => {
-  const buttonClassNames = cn('button', className, { [`button_${variant}`]: variant });
+export const Button: FC<ButtonProps> = ({ type = 'button', text, variant, testId, ...rest }) => {
+  const buttonClassNames = cn('button', {
+    button_primary: variant === ButtonVariant.primary,
+    button_secondary: variant === ButtonVariant.secondary,
+  });
 
   return (
     <button {...rest} type={type} className={buttonClassNames} data-testid={testId}>
       {text}
     </button>
   );
-  // return <button {...props}>{text}</button>;
 };
