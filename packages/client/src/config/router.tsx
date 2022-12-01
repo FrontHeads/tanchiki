f9f140e4e8adacc7b801df6dfe746ff6ff43be13
Optimizitor <sapomaro@gmail.com>
2022-12-01 17:05:01 +0300

    style: prettier import sort

--- packages/client/src/config/router.tsx
+++ packages/client/src/config/router.tsx
@@ -2,12 +2,12 @@ import { createBrowserRouter } from 'react-router-dom';
 
 import { Root } from '../layouts/Root';
 import { ErrorPage } from '../pages/ErrorPage';
+import { Game } from '../pages/Game';
 import { Home } from '../pages/Home';
 import { Leaderboard } from '../pages/Leaderboard';
 import { SignIn } from '../pages/SignIn';
 import { SignUp } from '../pages/SignUp';
 import { UserProfile } from '../pages/UserProfile';
-import { Game } from '../pages/Game';
 import { Paths } from './constants';
 
 export const router = createBrowserRouter([
