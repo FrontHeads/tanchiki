072e88763f6d785a2157125ca3fef2c9d7c25023
DmitriiNazimov <kurs-ssu@mail.ru>
2022-12-01 12:09:57 +0300

    refactor(Button): refactor props notation

--- packages/client/src/components/Button/Button.tsx
+++ packages/client/src/components/Button/Button.tsx
@@ -5,14 +5,14 @@ import { FC } from 'react';
 
 import { ButtonProps, ButtonVariant } from './typings';
 
-export const Button: FC<ButtonProps> = ({ type = 'button', text, variant, testId, ...rest }) => {
+export const Button: FC<ButtonProps> = ({ text, variant, type = 'button', ...props }) => {
   const buttonClassNames = cn('button', {
     button_primary: variant === ButtonVariant.primary,
     button_secondary: variant === ButtonVariant.secondary,
   });
 
   return (
-    <button {...rest} type={type} className={buttonClassNames} data-testid={testId}>
+    <button {...props} type={type} className={buttonClassNames}>
       {text}
     </button>
   );
