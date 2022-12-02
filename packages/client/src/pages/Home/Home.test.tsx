7334e2bac9c73f5156134e5c6dd770c241f7214c
andreyatx <mitrofanobandrei@gmail.com>
2022-12-02 14:20:33 +0300

    test(Home.test.tsx): test fix

--- packages/client/src/pages/Home/Home.test.tsx
+++ packages/client/src/pages/Home/Home.test.tsx
@@ -1,12 +1,20 @@
 import '@testing-library/jest-dom';
 
 import { screen } from '@testing-library/react';
+import axios from 'axios';
+import MockAdapter from 'axios-mock-adapter';
 
 import { navigationList } from '../../components/Menu/MenuData';
+import { buildPath } from '../../utils/HTTP';
 import { renderWithRouter } from '../../utils/testing-utils';
 import { Home } from './Home';
 
 describe('Home page', () => {
+  beforeAll(() => {
+    const mock = new MockAdapter(axios);
+    mock.onGet(buildPath(`http://localhost:${__SERVER_PORT__}`, '/')).reply(200);
+  });
+
   test('it renders', () => {
     renderWithRouter(<Home />);
 
