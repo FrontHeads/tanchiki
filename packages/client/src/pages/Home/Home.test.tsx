75986f3db425eccd025ca596110f973418785efb
andreyatx <mitrofanobandrei@gmail.com>
2022-12-03 12:11:30 +0300

    test(Home.test.tsx): add axios mock

--- packages/client/src/pages/Home/Home.test.tsx
+++ packages/client/src/pages/Home/Home.test.tsx
@@ -1,12 +1,25 @@
 import '@testing-library/jest-dom';
 
 import { screen } from '@testing-library/react';
+import MockAdapter from 'axios-mock-adapter';
 
 import { navigationList } from '../../components/Menu/MenuData';
+import { axios, buildPath } from '../../utils/HTTP';
 import { renderWithRouter } from '../../utils/testing-utils';
 import { Home } from './Home';
 
+jest.mock('react-router-dom', () => {
+  return {
+    ...jest.requireActual('react-router-dom'),
+    useNavigation: jest.fn(() => ({ state: '' })),
+  };
+});
+
 describe('Home page', () => {
+  beforeAll(() => {
+    const mock = new MockAdapter(axios);
+    mock.onGet(buildPath(`http://localhost:${__SERVER_PORT__}`, '/')).reply(200);
+  });
   test('it renders', () => {
     renderWithRouter(<Home />);
 
