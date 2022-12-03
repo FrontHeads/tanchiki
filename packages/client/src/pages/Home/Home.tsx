44b38993d363e7cd5deb2f156943d467bcf9ff20
andreyatx <mitrofanobandrei@gmail.com>
2022-12-03 16:39:39 +0300

    feat(Home): add logout link

--- packages/client/src/pages/Home/Home.tsx
+++ packages/client/src/pages/Home/Home.tsx
@@ -3,11 +3,14 @@ import './Home.css';
 import { FC, useEffect } from 'react';
 
 import promoImg from '../../assets/img/tankettes_game.png';
-import { navigationList } from '../../components/Menu/MenuData';
+import { logoutItem, navigationList } from '../../components/Menu/MenuData';
 import { MenuLink } from '../../components/MenuLink';
+import { authThunks, useAppDispatch } from '../../store';
 import { HTTP } from '../../utils/HTTP';
 
 export const Home: FC = () => {
+  const dispatch = useAppDispatch();
+
   useEffect(() => {
     const fetchServerData = async () => {
       const data = await HTTP.get<Record<string, unknown>>('/', {
@@ -19,6 +22,16 @@ export const Home: FC = () => {
     fetchServerData();
   }, []);
 
+  const logoutHandler = () => {
+    dispatch(authThunks.logout());
+  };
+
+  const menuLinks = navigationList.map(({ id, title, to }) => <MenuLink key={id} title={title} to={to} />);
+
+  const logoutLink = (
+    <MenuLink handleNavigate={logoutHandler} key={logoutItem.id} title={logoutItem.title} to={logoutItem.to} />
+  );
+
   return (
     <>
       <img src={promoImg} alt="Игра Танчики на Денди" className="promo-img" />
@@ -26,9 +39,10 @@ export const Home: FC = () => {
         <div className="delimiter" />
         <nav data-testid="menu-nav-home" className="menu-nav">
           <ul className="navigation-list">
-            {navigationList.map(({ id, title, to }) => (
-              <MenuLink key={id} title={title} to={to} />
-            ))}
+            <>
+              {menuLinks}
+              {logoutLink}
+            </>
           </ul>
         </nav>
       </div>
