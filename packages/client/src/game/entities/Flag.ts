615f1ae9d36e2776749960c4e3991cd2df61324f
Optimizitor <sapomaro@gmail.com>
2022-12-03 22:34:53 +0300

    feat: add flag

---
+++ packages/client/src/game/entities/Flag.ts
@@ -0,0 +1,14 @@
+import type { EntitySettings } from '../typings';
+import { Entity } from './';
+
+export class Flag extends Entity {
+  constructor(props: EntitySettings) {
+    super(props);
+    Object.assign(this, props);
+    this.role = 'player1';
+    this.type = 'flag';
+    this.color = 'white';
+    this.crossable = false;
+    this.hittable = true;
+  }
+}
