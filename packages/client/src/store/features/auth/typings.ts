4e0b95f4e2721f39e87ca02fb914afedaa68d348
Stanislav Salnikov <delfi89@gmail.com>
2022-12-05 15:41:59 +0100

    fix: fix authSlice typings

--- packages/client/src/store/features/auth/typings.ts
+++ packages/client/src/store/features/auth/typings.ts
@@ -2,6 +2,7 @@ import { UserProfile } from '../../../app.typings';
 
 export interface AuthState {
   isLoading: boolean;
+  isAuthenticated: boolean;
   error: string;
   userProfile: UserProfile | null;
 }
