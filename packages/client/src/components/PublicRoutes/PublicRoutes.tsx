602b22d28fb8d99c071a642e50027416b636c63c
Stanislav Salnikov <delfi89@gmail.com>
2022-12-05 11:52:44 +0100

    refactor(PublicRoutes): add PublicRoute notification

--- packages/client/src/components/PublicRoutes/PublicRoutes.tsx
+++ packages/client/src/components/PublicRoutes/PublicRoutes.tsx
@@ -1,5 +1,6 @@
 import { FC } from 'react';
 import { Navigate, Outlet, useLocation } from 'react-router-dom';
+import { toast } from 'react-toastify';
 
 import { Paths } from '../../config/constants';
 import { authSelectors, useAppSelector } from '../../store';
@@ -9,6 +10,7 @@ export const PublicRoutes: FC = () => {
   const location = useLocation();
 
   if (auth) {
+    toast.success('Вы авторизованы!');
     const to = location.state?.from?.pathname || Paths.Home;
     return <Navigate to={to} replace={true} />;
   }
