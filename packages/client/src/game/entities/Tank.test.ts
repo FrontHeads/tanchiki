e4cebf49964072483ade3e6dd9e0b214ae77cd27
DmitriiNazimov <kurs-ssu@mail.ru>
2022-12-09 13:13:22 +0300

    test(Tank.test): add shoot lag tests

--- packages/client/src/game/entities/Tank.test.ts
+++ packages/client/src/game/entities/Tank.test.ts
@@ -1,3 +1,4 @@
+import { Zone } from '../services';
 import { Direction } from '../typings';
 import { Projectile, Tank } from './';
 
@@ -19,5 +20,38 @@ describe('game/entities/Tank', () => {
     expect(projectile.height).toBe(2);
     expect(projectile.posX).toBe(2);
     expect(projectile.posY).toBe(4);
+  });
+
+  it('shouldn`t shoot before projectile exploded', () => {
+    const tank = new Tank({ posX: 2, posY: 2, width: 2, height: 2, direction: Direction.DOWN });
+
+    const projectileOne = tank.shoot() as Projectile;
+    const projectileTwo = tank.shoot() as Projectile;
+
+    expect(projectileOne).toBeTruthy();
+    expect(projectileTwo).toBeFalsy();
+  });
+
+  it('should shoot after projectile exploded', async () => {
+    const zone = new Zone({ width: 4, height: 4 });
+    const tank = new Tank({ posX: 1, posY: 1, width: 1, height: 1, direction: Direction.DOWN });
+
+    const projectileOne = tank.shoot() as Projectile;
+    const projectileTwo = tank.shoot() as Projectile;
+
+    expect(projectileOne).toBeTruthy();
+    expect(projectileTwo).toBeFalsy();
+
+    zone.registerEntity(projectileOne);
+    projectileOne.spawn({ posX: 2, posY: 2 });
+
+    projectileOne.update();
+    projectileOne.update();
+    projectileOne.update();
+
+    expect(projectileOne).toHaveProperty('shouldExplode', true);
+
+    const projectileThree = tank.shoot() as Projectile;
+    expect(projectileThree).toBeTruthy();
   });
 });
