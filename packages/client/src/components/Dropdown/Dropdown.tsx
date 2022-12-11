e7347b3f81de2e3baccdfc3c08b8d9885707184e
Dmitry Gramoteev <gramoteev@gmail.com>
2022-12-11 19:22:39 +0500

    refactor(Dropdown): update by pr comments

--- packages/client/src/components/Dropdown/Dropdown.tsx
+++ packages/client/src/components/Dropdown/Dropdown.tsx
@@ -5,36 +5,42 @@ import { cloneElement, FC, useEffect, useRef, useState } from 'react';
 import { DropdownProps } from './typings';
 
 export const Dropdown: FC<DropdownProps> = ({ trigger, menuItems }) => {
-  const [open, setOpen] = useState(false);
-  const menuRef = useRef<HTMLDivElement>(null);
+  const [isOpen, setIsOpen] = useState(false);
+  const dropdownRef = useRef<HTMLDivElement>(null);
 
+  /** При добавлении компонента Dropdown на страницу подписываемся на клик мышкой,
+   * если клик был совершен снаружи dropdown, то закрываем его.
+   * Когда Dropdown исчезает со страницы, выполняем return callback
+   * */
   useEffect(() => {
     const handler = (e: MouseEvent) => {
-      if (!menuRef.current?.contains(e.target as HTMLElement)) {
-        setOpen(false);
+      if (!dropdownRef.current?.contains(e.target as HTMLElement)) {
+        setIsOpen(false);
       }
     };
     document.addEventListener('mousedown', handler);
     return () => document.removeEventListener('mousedown', handler);
-  }, [menuRef]);
+  }, [dropdownRef]);
 
-  const handleOpen = () => {
-    setOpen(!open);
+  const toggleIsOpen = () => {
+    setIsOpen(!isOpen);
   };
 
+  /** С помощью функции cloneElement пробрасываем элемент trigger с целевой страницы
+   *  и добавляем ему функцию-обработчик toggleIsOpen
+   * */
   return (
-    <div className="dropdown" ref={menuRef}>
+    <div className="dropdown" ref={dropdownRef}>
       {cloneElement(trigger, {
-        onClick: handleOpen,
-        'data-testid': 'dropdown-trigger',
+        onClick: toggleIsOpen,
       })}
-      {open ? (
+      {isOpen ? (
         <ul className="dropdown__menu">
           {menuItems.map(({ title, onClick, ...rest }, index) => (
             <li key={index} className="dropdown__menu-item">
               <button
                 onClick={event => {
-                  handleOpen();
+                  toggleIsOpen();
                   onClick(event);
                 }}
                 className="dropdown__menu-button"
