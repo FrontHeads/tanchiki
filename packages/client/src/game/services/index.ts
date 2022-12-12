bf32cc385125b2b12f4fc490f1803db3fa21223c
Stanislav <delfi89@gmail.com>
2022-12-12 10:19:46 +0100

    feat(game): Scenario implementation (#30)
    * feat: add level seletor page
    
    * feat(game): change overlay acrchitecture, add level selector screen
    
    * feat(game): add new screens, refacor typings
    
    * feat(game): add Screen test. Add levels date
    
    * feat(game): add level cells rendering
    
    * feat(Scenario): add enemy tanks
    
    * feat: add concrete wall entity type
    
    * refactor: change entity creation and shooting
    
    * feat(game): add new Sceneario features
    
    * refactor(Scenario): refactor scenario
    
    * fix(game): fix Game Scenario test
    
    * fix(game): fix Game Scenario test
    
    * docs(game): update Game readme
    
    * fix: eslint
    
    * style: prettier
    
    * fix: entity positioning, entering wall bug
    
    * style: prettier
    
    * refactor(game): fix issues from review
    
    Co-authored-by: Optimizitor <sapomaro@gmail.com>

--- packages/client/src/game/services/index.ts
+++ packages/client/src/game/services/index.ts
@@ -1,4 +1,6 @@
 export { Controller } from './Controller';
 export { Game } from './Game';
+export { MapManager } from './MapManager';
+export { Scenario } from './Scenario';
 export { View } from './View';
 export { Zone } from './Zone';
