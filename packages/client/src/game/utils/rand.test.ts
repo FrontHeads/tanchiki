26c6383639d5084e4b5b8e40c23c562d7dad7e60
Optimizitor <sapomaro@gmail.com>
2022-12-16 07:37:46 +0300

    test: update rand test

--- packages/client/src/game/utils/rand.test.ts
+++ packages/client/src/game/utils/rand.test.ts
@@ -2,6 +2,8 @@ import { rand } from './';
 
 describe('rand', () => {
   test('rand works properly', () => {
+    expect([1, 2].includes(rand(1, 2))).toBe(true);
+    expect([6, 7, 8, 9].includes(rand(6, 9))).toBe(true);
     expect(rand(1, 2)).toBeGreaterThan(0);
     expect(rand(1, 2)).toBeLessThan(3);
     expect(rand(4, 5)).toBeGreaterThan(3);
