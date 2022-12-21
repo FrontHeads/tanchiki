fccd8a42ef7e8c6429592ce5ec3fd01077a3b55c
Dmitry Gramoteev <gramoteev@gmail.com>
2022-12-21 13:15:45 +0500

    refactor(General): replace all "&&" to ternary operators

--- packages/client/src/components/Form/Form.tsx
+++ packages/client/src/components/Form/Form.tsx
@@ -7,7 +7,7 @@ import { FormProps } from './typings';
 export const Form: FC<FormProps> = ({ handlerSubmit, header = '', children, ...rest }) => {
   return (
     <form data-testid="form" className="form" onSubmit={handlerSubmit} {...rest}>
-      {header && <h1 className="form__header">{header}</h1>}
+      {header ? <h1 className="form__header">{header}</h1> : null}
       {children}
     </form>
   );
