483b0ed2cefe74a7a45c86a6f99bbdb8f9676487
Stanislav <delfi89@gmail.com>
2022-12-24 15:05:29 +0100

    feat(game): pause the game when a tab becomes inactive (#52)
    * feat(game): pause game when tab becomes inactive
    
    * refactor(game): fix issues from review

--- packages/client/src/pages/Game/Game.tsx
+++ packages/client/src/pages/Game/Game.tsx
@@ -3,14 +3,24 @@ import './Game.css';
 import { useEffect, useRef } from 'react';
 
 import { Tanchiki } from '../../game';
+import { ScreenType } from '../../game/typings';
+import { usePageVisibility } from '../../hooks/usePageVisibility';
 
 export const Game = () => {
   const gameRoot = useRef(null);
+  const isTabActive = usePageVisibility();
+  const game = Tanchiki.create();
 
   useEffect(() => {
-    const game = Tanchiki.create();
     game.init(gameRoot.current);
   }, []);
+
+  /** Если вкладка становится не активной, то ставим игру на паузу */
+  useEffect(() => {
+    if (isTabActive === false && !game.paused && game.screen === ScreenType.GAME) {
+      game.togglePause();
+    }
+  }, [isTabActive]);
 
   return <div ref={gameRoot} className="game__root"></div>;
 };
