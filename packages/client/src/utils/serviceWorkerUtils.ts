e3dd1884c6bc996507b5dfbef604d43980436018
Optimizitor <sapomaro@gmail.com>
2022-12-24 12:21:56 +0300

    refactor(serviceWorkerUtils.ts): add checkCompatibility method

--- packages/client/src/utils/serviceWorkerUtils.ts
+++ packages/client/src/utils/serviceWorkerUtils.ts
@@ -1,39 +1,37 @@
-export function registerServiceWorker(reporting = true) {
-  if ('serviceWorker' in navigator) {
-    navigator.serviceWorker
-      .register('/serviceWorker.js')
-      .then((registration) => {
-        if (reporting) {
-          console.log('ServiceWorker registration successful with scope: ', registration.scope);
-        }
-      })
-      .catch((error: string) => {
-        if (reporting) {
-          console.warn('ServiceWorker registration failed: ', error);
-        }
-      });
-  } else {
-    if (reporting) {
-      console.warn('ServiceWorker is not supported in the browser');
-    }
+function checkCompatibility(reporting = true) {
+  if (!('serviceWorker' in navigator)) {
+    reporting && console.warn('ServiceWorker is not supported');
+    return false;
   }
+  if (!caches) {
+    reporting && console.warn('Caches is not supported');
+    return false;
+  }
+  return true;
+}
+
+export function registerServiceWorker(reporting = true) {
+  if (!checkCompatibility(reporting)) {
+    return;
+  }
+  navigator.serviceWorker
+    .register('/serviceWorker.js')
+    .then(registration => {
+      reporting && console.log('SW registration successful with scope: ', registration.scope);
+    })
+    .catch((error: string) => {
+      reporting && console.warn('SW registration failed: ', error);
+    });
 }
 
 export function unregisterServiceWorker(reporting = true) {
-  if ('serviceWorker' in navigator) {
-    navigator.serviceWorker
-      .getRegistrations()
-      .then((registrations) => {
-        for (const registration of registrations) {
-          registration.unregister();
-          if (reporting) {
-            console.log('ServiceWorker unregistered: ', registration);
-          }
-        }
-      });
-  } else {
-    if (reporting) {
-      console.warn('ServiceWorker is not supported in the browser');
-    }
+  if (!checkCompatibility(reporting)) {
+    return;
   }
+  navigator.serviceWorker.getRegistrations().then(registrations => {
+    for (const registration of registrations) {
+      registration.unregister();
+      reporting && console.log('SW unregistered: ', registration);
+    }
+  });
 }
