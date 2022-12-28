bfa67e6bf7446ac475513d81e2820c5c361c0e66
Dmitry Gramoteev <gramoteev@gmail.com>
2022-12-29 00:15:26 +0500

    refactor(Form): remove unused code

--- packages/client/src/components/Form/typings.tsx
+++ packages/client/src/components/Form/typings.tsx
@@ -1,7 +1,6 @@
 import { FormEvent, FormHTMLAttributes } from 'react';
 
 export type FormProps = {
-  // hasErrors: boolean;
   header?: string;
   onSubmitHandler: (e: FormEvent<HTMLFormElement>) => void;
   children?: JSX.Element | JSX.Element[];
