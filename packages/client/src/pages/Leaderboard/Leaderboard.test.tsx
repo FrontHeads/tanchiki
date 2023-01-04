d3338abac57b8be7cf212b59487c26f772796b48
Stanislav Salnikov <delfi89@gmail.com>
2023-01-04 19:40:48 +0100

    fix(leaderboard): update test

--- packages/client/src/pages/Leaderboard/Leaderboard.test.tsx
+++ packages/client/src/pages/Leaderboard/Leaderboard.test.tsx
@@ -1,11 +1,12 @@
 import '@testing-library/jest-dom';
 
-import { render, screen } from '@testing-library/react';
+import { screen } from '@testing-library/react';
 
+import { renderWithRouter } from '../../utils/testingUtils';
 import { headerText, Leaderboard } from './Leaderboard';
 describe('Leaderboard', () => {
   test('it renders', () => {
-    render(<Leaderboard header={headerText} />);
+    renderWithRouter({ component: <Leaderboard header={headerText} /> });
 
     const renderedLeaderboard = screen.getByText(headerText);
 
