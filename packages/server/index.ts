20c6beb55b279acc6a61fe42fd3a9879de05225f
Optimizitor <sapomaro@gmail.com>
2023-01-18 22:21:14 +0300

    refactor(server): update comment

--- packages/server/index.ts
+++ packages/server/index.ts
@@ -47,12 +47,13 @@ async function startServer() {
 
   /** Проксирует запросы к API на сервер Яндекса */
   app.use('/api', (req, res, next) => {
-    // Если обращение к API идёт из незнакомого места - заворачиваем
+    // Если обращение к API идёт из незнакомого места - отклоняем
     if (!allowedHosts.includes(req.hostname)) {
       res.statusCode = 502;
       res.send('<!doctype html><p>Bad gateway</p>');
       return;
     }
+
     return createProxyMiddleware({
       target: 'https://ya-praktikum.tech/api/v2',
       pathRewrite: { '^/api': '' }, // чтобы в конец пути target не добавлялось лишнее /api
