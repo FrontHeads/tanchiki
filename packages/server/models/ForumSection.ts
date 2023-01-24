80a11bd1fab999089e36aa9535ff3d324c8fd996
Dmitry Gramoteev <gramoteev@gmail.com>
2023-01-24 15:58:37 +0500

    refactor(forum-backend): add username instead of User, fix dates naming

--- packages/server/models/ForumSection.ts
+++ packages/server/models/ForumSection.ts
@@ -2,7 +2,7 @@ import { AllowNull, Column, HasMany, Model, Table } from 'sequelize-typescript';
 
 import { ForumTopic } from './ForumTopic';
 
-@Table({ tableName: 'forum_sections' })
+@Table({ tableName: 'forum_sections', createdAt: 'created_date', updatedAt: 'updated_date' })
 export class ForumSection extends Model {
   @AllowNull(false)
   @Column
