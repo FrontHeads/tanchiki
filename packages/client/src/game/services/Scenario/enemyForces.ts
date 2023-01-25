47d23701a4e4ecd8bae46ce7787c923e42a175b8
Dmitry Gramoteev <gramoteev@gmail.com>
2023-01-25 22:22:41 +0500

    feat(enemyForces): add enemyForces

---
+++ packages/client/src/game/services/Scenario/enemyForces.ts
@@ -0,0 +1,50 @@
+export const enemyForces = [
+  // 1~5
+  'aaaaaaaaaaaaaaaaaabb',
+  'aaaaabaaabaaabaabdad',
+  'aaaaabaaabaaabaabdad',
+  'acbacbcbccbccdccbcdd',
+  'aabcabaabcaacbacbdcd',
+
+  // 6~10
+  'aabcacacacabadcacacd',
+  'aabaacbacacaacbacabc',
+  'cacacbaacbaacbdacbcd',
+  'cabaabacdacbacdcbcdc',
+  'aabacaadabcaaacaacad',
+
+  // 11~15
+  'bcbdbbdcbdbcbdbbdcbd',
+  'cbbcddcbdcbdcbdcbcdc',
+  'cbbbcdbcbdcbdcbcbcdc',
+  'bccbcbccbdcdcdcdcdcd',
+  'badbabdbbdbbdbdbdbdd',
+
+  // 16~20
+  'aaabaaaabaaadaaaaaad',
+  'aadbdadadadbdadadadd',
+  'cbacbabcbdcbbcdbcdbd',
+  'abcdabcdabcdabcddddd',
+  'abcdcdcdcdabcdcdcdcd',
+
+  // 21~25
+  'cabcacacdcabcdacdacd',
+  'abcabdabadbcabbdabbd',
+  'cbbbcbbdbbdcbdbdbdcd',
+  'abcaacbaaabcaacbdaad',
+  'bbdbddbcdbdbdcbddbdd',
+
+  // 26~30
+  'abcdcdabcdabcdabcdbd',
+  'acdcdcdcdacdcdbcdbcd',
+  'aaaaaaaaaaababaadcac',
+  'bcbdccbdccdcdbcdcccd',
+  'babcbdbabbcbdbabbcdb',
+
+  // 31~35
+  'bcdbbcdbcbdbcdbbcdcd',
+  'cabdaadbadadbdadbdcd',
+  'cbbdcbdbcdbbdcddbdbd',
+  'bbcbcbbbdbdbcdbdbdcd',
+  'cbdbdbdbdbddbcddcdcd',
+];
