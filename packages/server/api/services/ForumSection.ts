867adbf523d19c819f0722f93dd8397c3d764a26
Dmitry Gramoteev <gramoteev@gmail.com>
2023-01-27 15:00:45 +0500

    feat(forum): add topics to section, return id on topic create

--- packages/server/api/services/ForumSection.ts
+++ packages/server/api/services/ForumSection.ts
@@ -1,18 +1,25 @@
 import express, { type Request, type Response, Router } from 'express';
+import { Sequelize } from 'sequelize-typescript';
 
 import { ForumSection } from '../../models/ForumSection';
+import { ForumTopic } from '../../models/ForumTopic';
 import { throwIf } from '../../utils/throwIf';
 
 export const forumSectionRoute = Router()
   .use(express.json())
   .use(express.urlencoded({ extended: true }))
   .get('/', (_: Request, res: Response, next) => {
-    ForumSection.findAll()
+    ForumSection.findAll({
+      subQuery: false,
+      attributes: { include: [[Sequelize.fn('COUNT', Sequelize.col('topics.id')), 'topicCount']] },
+      include: { model: ForumTopic, attributes: [] },
+      group: ['ForumSection.id'],
+    })
       .then((sections: ForumSection[]) => res.status(200).json(sections))
       .catch(next);
   })
   .get('/:id', (req: Request, res: Response, next) => {
-    ForumSection.findByPk(req.params.id)
+    ForumSection.findByPk(req.params.id, {include: ForumTopic})
       .then(throwIf(r => !r, res, 400, 'Категория не найдена'))
       .then(section => res.status(200).json(section))
       .catch(next);
