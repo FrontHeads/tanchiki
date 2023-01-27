c82c1c328856835183fe780843416b609c33b34c
Optimizitor <sapomaro@gmail.com>
2023-01-27 21:54:55 +0300

    feat(game): add enemy tank variants (#81)
    Добавлены новые виды вражеских танков с соответствующими характеристиками.
    
    ---------
    
    Co-authored-by: Dmitry Gramoteev <gramoteev@gmail.com>

--- packages/client/src/game/entities/Tank/typings.ts
+++ packages/client/src/game/entities/Tank/typings.ts
@@ -1,3 +1,7 @@
+import { type EntityDynamicSettings } from '../EntityDynamic/typings';
+
 export type PlayerVariant = 'PLAYER1' | 'PLAYER2';
 
 export type EnemyVariant = 'BASIC' | 'FAST' | 'POWER' | 'ARMOR';
+
+export type TankEnemySettings = { variant: EnemyVariant } & Partial<EntityDynamicSettings>;
