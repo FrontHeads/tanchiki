806d8bc16031da1f7026fd94445cc3049ddf68a0
andreyatx <mitrofanobandrei@gmail.com>
2023-02-05 19:32:20 +0300

    refactor(forumThunks): remove unused thunks

--- packages/client/src/store/features/forum/forumThunks.ts
+++ packages/client/src/store/features/forum/forumThunks.ts
@@ -1,32 +1,11 @@
 import { createAsyncThunk } from '@reduxjs/toolkit';
 
-import { type createTopicRequestData, forumAPI } from '../../../api/forumAPI';
+import { forumAPI } from '../../../api/forumAPI';
 import { type MessageRequest } from './typings';
-
-const createTopic = createAsyncThunk('forum/topic', async (payload: createTopicRequestData) => {
-  const data = await forumAPI.createTopic(payload);
-
-  return data;
-});
-
-const getTopicsFromSection = createAsyncThunk('forum/topic/section_id', async (sectionId?: number) => {
-  const { data } = await forumAPI.getTopicsFromSection(sectionId);
-  return data;
-});
-
-const getAllSections = createAsyncThunk('forum/sections', async () => {
-  const { data } = await forumAPI.getAllSections();
-  return data;
-});
 
 const createMessage = createAsyncThunk('forum/message', async (message: MessageRequest) => {
   const { data } = await forumAPI.createMessage(message);
   return data;
 });
 
-// const editMessage = createAsyncThunk('forum/message/edit', async (messageId: number, message: MessageRequest) => {
-//   const { data } = await forumAPI.editMessage(messageId, message);
-//   return data;
-// });
-
-export const forumThunks = { getAllSections, createTopic, getTopicsFromSection, createMessage };
+export const forumThunks = { createMessage };
