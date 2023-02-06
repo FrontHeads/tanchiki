ad10dbf609ab1bb02d6d15ddfc8eb08a0b880741
Stanislav Salnikov <delfi89@gmail.com>
2023-02-06 17:13:44 +0100

    fix(forum): fix ForumTopic test

--- packages/client/src/tests/data.ts
+++ packages/client/src/tests/data.ts
@@ -1,5 +1,7 @@
 import { type UserDTO } from '../api/typings';
 import { type ForumSectionT } from '../pages/Forum/ForumSection/typings';
+import { type User } from '../pages/Forum/ForumTopic/ForumMessage/typings';
+import { type ForumTopicT } from './../pages/Forum/ForumTopic/typings';
 
 export const fakeUserProfile: UserDTO = {
   id: 1,
@@ -12,7 +14,7 @@ export const fakeUserProfile: UserDTO = {
   phone: '555555',
 };
 
-export const fakeSectionData: ForumSectionT = {
+export const fakeForumSectionData: ForumSectionT = {
   id: 1,
   name: 'Новости',
   topics: [
@@ -22,4 +24,37 @@ export const fakeSectionData: ForumSectionT = {
       messages: 10,
     },
   ],
+};
+
+const fakeUser: User = {
+  id: 1,
+  avatar: '',
+  login: 'user1',
+  display_name: 'User display name',
+  created_at: '',
+  updated_at: '',
+};
+
+export const fakeForumTopicData: ForumTopicT = {
+  id: 1,
+  content: 'Тестовый топик',
+  created_at: '',
+  messages: [
+    {
+      content: 'Коммент 1',
+      id: 1,
+      created_at: '',
+      updated_at: '',
+      topic_id: 1,
+      user_id: 1,
+      user: fakeUser,
+    },
+  ],
+  name: 'Топик 1',
+  section: fakeForumSectionData,
+  section_id: 1,
+  updated_at: '',
+  user_id: 1,
+  user: fakeUser,
+  username: 'user1',
 };
--- packages/client/src/tests/setup.ts
+++ packages/client/src/tests/setup.ts
@@ -4,7 +4,7 @@ import MockAdapter from 'axios-mock-adapter';
 
 import { API_ENDPOINTS, LOCAL_API_HOST } from '../config/constants';
 import { axios, buildPath } from '../utils/HTTP';
-import { fakeSectionData, fakeUserProfile } from './data';
+import { fakeForumSectionData, fakeForumTopicData, fakeUserProfile } from './data';
 
 const mock = new MockAdapter(axios);
 mock.onGet(buildPath(`http://localhost:${__SERVER_PORT__}`, '/')).reply(200);
@@ -14,7 +14,8 @@ mock.onGet(buildPath(LOCAL_API_HOST, API_ENDPOINTS.THEMIZATION)).reply(200);
 mock.onPost(buildPath(LOCAL_API_HOST, API_ENDPOINTS.AUTH.SIGNIN)).reply(200);
 mock.onPost(buildPath(LOCAL_API_HOST, API_ENDPOINTS.AUTH.SIGNUP)).reply(200);
 mock.onPost(buildPath(LOCAL_API_HOST, API_ENDPOINTS.AUTH.LOGOUT)).reply(200);
-mock.onGet(buildPath(LOCAL_API_HOST, API_ENDPOINTS.FORUM.GET_SECTION_BY_ID(1))).reply(200, fakeSectionData);
+mock.onGet(buildPath(LOCAL_API_HOST, API_ENDPOINTS.FORUM.GET_SECTION_BY_ID(1))).reply(200, fakeForumSectionData);
+mock.onGet(buildPath(LOCAL_API_HOST, API_ENDPOINTS.FORUM.GET_TOPIC_BY_ID(1))).reply(200, fakeForumTopicData);
 
 jest.mock('react-router-dom', () => {
   return {
