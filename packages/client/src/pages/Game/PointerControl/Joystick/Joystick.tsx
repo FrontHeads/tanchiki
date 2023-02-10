fa9c9f0515c803ecb0d17a3f542beae7977c5d6c
DmitriiNazimov <kurs-ssu@mail.ru>
2023-02-10 03:26:23 +0300

    fix: arrows icons view problem

--- packages/client/src/pages/Game/PointerControl/Joystick/Joystick.tsx
+++ packages/client/src/pages/Game/PointerControl/Joystick/Joystick.tsx
@@ -9,19 +9,27 @@ export const Joystick: FC = () => {
   return (
     <div className="joystick" data-testid="joystick">
       <div className="joystick__up joystick__vertical-buttons">
-        <button className={`joystick__button ${ControllerBtnClassName.MoveUp}`}>▲</button>
+        <button className={`joystick__button ${ControllerBtnClassName.MoveUp}`}>
+          <span className={`${ControllerBtnClassName.MoveUp}_arrow`}>▲</span>
+        </button>
       </div>
       <div className="joystick__row">
         <div className="joystick__left joystick__horizontal-buttons">
-          <button className={`joystick__button ${ControllerBtnClassName.MoveLeft}`}>◀</button>
+          <button className={`joystick__button ${ControllerBtnClassName.MoveLeft}`}>
+            <div className={`${ControllerBtnClassName.MoveLeft}_arrow`}>▲</div>
+          </button>
         </div>
         <div className="joystick__center" data-testid="joystick__center"></div>
         <div className="joystick__right joystick__horizontal-buttons">
-          <button className={`joystick__button ${ControllerBtnClassName.MoveRight}`}>▶</button>
+          <button className={`joystick__button ${ControllerBtnClassName.MoveRight}`}>
+            <span className={`${ControllerBtnClassName.MoveRight}_arrow`}>▲</span>
+          </button>
         </div>
       </div>
       <div className="joystick__bottom joystick__vertical-buttons">
-        <button className={`joystick__button ${ControllerBtnClassName.MoveDown}`}>▼</button>
+        <button className={`joystick__button ${ControllerBtnClassName.MoveDown}`}>
+          <span className={`${ControllerBtnClassName.MoveDown}_arrow`}>▲</span>
+        </button>
       </div>
     </div>
   );
