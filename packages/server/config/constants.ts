5546907028f09b335a242c452cd06e24116b25ca
andreyatx <mitrofanobandrei@gmail.com>
2023-02-12 12:02:18 +0300

    chore: remove comments

--- packages/server/config/constants.ts
+++ packages/server/config/constants.ts
@@ -5,28 +5,3 @@ export const allowedHosts = ['localhost', '127.0.0.1'];
 
 export const NONCE = '%nonce%';
 export const TLD = '%tld%';
-
-// export {
-//   ALLOW_FORMS,
-//   ALLOW_MODALS,
-//   ALLOW_ORIENTATION_LOCK,
-//   ALLOW_POINTER_LOCK,
-//   ALLOW_POPUPS,
-//   ALLOW_POPUPS_TO_ESACPE_SANDBOX,
-//   ALLOW_PRESENTATION,
-//   ALLOW_SAME_ORIGIN,
-//   ALLOW_SCRIPTS,
-//   ALLOW_TOP_NAVIGATION,
-//   BLOB,
-//   DATA,
-//   EVAL,
-//   INLINE,
-//   NO_REFERER,
-//   NONE,
-//   NONE_WHEN_DOWNGRADE,
-//   ORIGIN,
-//   ORIGIN_WHEN_CROSS_ORIGIN,
-//   SELF,
-//   STRICT_DYNAMIC,
-//   UNSAFE_URL,
-// } from 'csp-header';
