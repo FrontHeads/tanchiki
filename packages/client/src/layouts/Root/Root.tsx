b49489a02c550c93f9bb2d7f4fb951aabc36e2d9
Dmitry Gramoteev <gramoteev@gmail.com>
2023-02-20 18:52:53 +0500

    fix(OAuth): fix OAuth work

--- packages/client/src/layouts/Root/Root.tsx
+++ packages/client/src/layouts/Root/Root.tsx
@@ -1,6 +1,6 @@
 import './Root.css';
 
-import { type FC, Suspense } from 'react';
+import { type FC, Suspense, useEffect } from 'react';
 import { Await, Outlet, ScrollRestoration, useLoaderData, useLocation } from 'react-router-dom';
 import { toast, ToastContainer } from 'react-toastify';
 
@@ -10,15 +10,28 @@ import { ErrorBoundary } from '../../components/ErrorBoundary';
 import { Footer } from '../../components/Footer';
 import { Loader } from '../../components/Loader';
 import { Logo } from '../../components/Logo';
-import { Paths } from '../../config/constants';
-import { appSelectors, useAppSelector } from '../../store';
+import { PATH, Paths } from '../../config/constants';
+import { appSelectors, oauthThunks, useAppDispatch, useAppSelector } from '../../store';
 import { type ResponseType } from '../../utils/HTTP';
 
 export const Root: FC = () => {
   const isAppLoading = useAppSelector(appSelectors.isAppLoading);
   const location = useLocation();
   const printHeaderAndFooter = location?.pathname !== Paths.Game;
+  const dispatch = useAppDispatch();
+
   const loaderData = useLoaderData() as { user: Promise<ResponseType<UserProfile>> };
+
+  useEffect(() => {
+    const oauthCode = new URLSearchParams(window.location.search).get('code');
+    if (oauthCode) {
+      // Меняем url страницы на чистый, без code
+      window.history.pushState({}, '', PATH.oauthRedirect);
+
+      // Отправляем запрос на oauth авторизацию
+      dispatch(oauthThunks.signInOauth(oauthCode));
+    }
+  }, []);
 
   return (
     <ErrorBoundary>
