8a45e60dbf189c881ec1e62ff77d0e0849cf8efa
Gramoteev <51950012+Gramoteev@users.noreply.github.com>
2023-02-21 10:11:45 +0500

    feat: Improve form library (#99)
    * feat(Form): update functionality
    
    * feat(Form): update functionality
    
    * feat(Form): add scroll to invalid field
    
    * fix(Form): tests
    
    * refactor(index.css): remove unused css property
    
    * refactor(UserProfile): remove unused hidingFields property
    
    * refactor(validation/config): pass '_' symbol on email
    
    * refactor(Field): remove redundant useMemo hook
    
    ---------
    
    Co-authored-by: Stanislav <delfi89@gmail.com>

--- packages/client/src/components/Form/FieldList/typings.ts
+++ packages/client/src/components/Form/FieldList/typings.ts
@@ -1,16 +1,13 @@
 import { type Dispatch, type SetStateAction } from 'react';
 
 import { type FormInputAndHeadingList } from '../../../app.typings';
-import { type useValidation } from '../../../utils/validation';
 
 export type FieldListProps<T> = {
   setFile?: Dispatch<File>;
   fieldList: FormInputAndHeadingList;
+  hidingFields?: Record<string, boolean>;
   formData: T;
   setFormData: Dispatch<SetStateAction<T>>;
   disabled?: boolean;
-  isFormSubmitted: boolean;
-  validation: ReturnType<typeof useValidation>;
   onFormSubmitCallback: () => void;
-  setIsFormSubmitted: (value: boolean) => void;
 };
